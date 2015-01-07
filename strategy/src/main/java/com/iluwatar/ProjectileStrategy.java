04ff8f3885d0acd95c4c90fc1198fb474f62add0
Robin Joshi <robin.joshi@selu.edu>
2015-01-07 15:51:43 -0600

    Update ProjectileStrategy.java

--- strategy/src/main/java/com/iluwatar/ProjectileStrategy.java
+++ strategy/src/main/java/com/iluwatar/ProjectileStrategy.java
@@ -4,8 +4,7 @@ public class ProjectileStrategy implements DragonSlayingStrategy {
 
 	@Override
 	public void execute() {
-		System.out
-				.println("You shoot the dragon with the magical crossbow and it falls dead on the ground!");
+		System.out.println("You shoot the dragon with the magical crossbow and it falls dead on the ground!");
 	}
 
 }
